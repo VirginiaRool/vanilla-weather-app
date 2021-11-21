fa61f76019ee27ee80510c8fb92c9814a8356e77
VirginiaRool <virginia.aro@gmail.com>
2021-11-20 23:13:35 -0600

    Display the forecast data using the API

--- src/script.js
+++ src/script.js
@@ -20,33 +20,50 @@ function formatDate(timestamp) {
     "Friday",
     "Saturday",
   ];
+
   let dayName = days[date.getDay()];
   return `${dayName}, ${dayHour}:${dayMinutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-  let days = ["Sat", "Sun", "Mon", "Thu"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 5) {
+      forecastHTML =
+        forecastHTML +
+        `
         <div class="col-2">
-          <div class="weather-forecast-date">${day}</div>
+          <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
           <img
-            src="http://openweathermap.org/img/wn/01n@2x.png"
+            src="http://openweathermap.org/img/wn/${
+              forecastDay.weather[0].icon
+            }@2x.png"
             alt=""
             width="36"
           />
           <div class="forecast-temperatures">
-            <span class="forecast-temperature-max">18°</span>
-            <span class="forecast-temperature-min">12°</span>
+            <span class="forecast-temperature-max">${Math.round(
+              forecastDay.temp.max
+            )}°</span>
+            <span class="forecast-temperature-min">${Math.round(
+              forecastDay.temp.min
+            )}°</span>
           </div>
         </div>x
       `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
